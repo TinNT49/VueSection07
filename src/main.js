f1410f54b1edd0ff478e9ce838ae2b9ced7b0770
TinNT49 <42561858+TinNT49@users.noreply.github.com>
2023-03-11 18:26:22 +0700

    20230311_Commit code for lession 100 section 08

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 import FriendContact from "./components/FriendContact.vue";
+import AddFriendContact from "./components/AddFriendContact.vue";
 
 const app = createApp(App);
 app.component("friend-contact", FriendContact);
+app.component("add-friend-contact", AddFriendContact);
 
 app.mount("#app");
